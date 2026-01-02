d0f4c4ddd0b268930b9142f6b9d1e70868f67ded
PaulShervin <paulshervin77@gmail.com>
2026-01-02 20:06:38 +0530

    done

--- easy/961. N-Repeated_Element.java
+++ easy/961. N-Repeated_Element.java
@@ -1,5 +1,6 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
+        //jan 2
         for(int i=0;i<nums.length-2;i++){
             if(nums[i]==nums[i+1]|| nums[i]==nums[i+2]){
                 return nums[i];
