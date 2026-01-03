dc8c2c76fe1a9c74437283c781df780b5c009b0f
PaulShervin <paulshervin77@gmail.com>
2026-01-03 10:52:52 +0530

    done

--- hard/1411. Number of Ways to Paint N × 3 Grid.java
+++ hard/1411. Number of Ways to Paint N × 3 Grid.java
@@ -24,3 +24,19 @@ class Solution {
 // 8.For each new row, valid transitions update A and B.
 // 9.The final answer is A + B, modulo 1e9 + 7.
 //jan 3
+
+
+
+// dp soluyion also added  in js 
+const m = 1e9 + 7;
+const numOfWays = (n) => {
+    let diff = 6;
+    let same = 6;
+    for (let i = 1; i < n; ++i) {
+        let newDiff = (2 * diff + 2 * same) % m;
+        let newSame = (2 * diff + 3 * same) % m;
+        same = newSame;
+        diff = newDiff;
+    }
+    return (same + diff) % m;
+};
