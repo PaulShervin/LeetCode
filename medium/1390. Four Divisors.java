b889dbcd3d5cae6fb01fa578d0d568c0fe292793
PaulShervin <paulshervin77@gmail.com>
2026-01-04 22:08:34 +0530

    done

---
+++ medium/1390. Four Divisors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int sumFourDivisors(int[] nums)
+    {
+        int ans=0;
+        for(int n:nums){
+            int sum=1+n;
+            int pair=0;
+            for(int i=2;i*i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    int j=n/i;
+
+                    if(i==j){
+                        pair=2;
+                        break;
+                    }
+                    sum+=i+j;
+                    pair++;
+                    if(pair>1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(pair==1){
+                ans+=sum;
+            }
+        }
+        return ans;
+    }
+}
