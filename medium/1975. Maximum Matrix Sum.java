51ec7bdad4511d780b0b1a9a3431cd76eed93bb9
PaulShervin <paulshervin77@gmail.com>
2026-01-05 22:49:07 +0530

    done

--- medium/1975. Maximum Matrix Sum.java
+++ medium/1975. Maximum Matrix Sum.java
@@ -24,3 +24,20 @@ class Solution {
     }
 }
 //jan 5 
+// Key Observations
+
+// Sign flipping does not change absolute value
+
+// Any number can be made positive or negative.
+
+// Therefore, the maximum possible sum is the sum of absolute values of all elements.
+
+// Parity of negative numbers matters
+
+// If the number of negative elements is even, all values can be converted to positive.
+
+// If the number of negative elements is odd, one element must remain negative.
+
+// Optimal element to keep negative
+
+// To minimize loss, keep the element with the smallest absolute value as negative.
