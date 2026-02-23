0341aee4822d8ad6a4dffcc44067ce7554273721
PaulShervin <paulshervin77@gmail.com>
2026-02-23 18:29:08 +0530

    Time: 4 ms (99.71%) | Memory: 46.2 MB (33.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,23 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-        {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        String str=String.valueOf(x);
-        int left=0;
-        int right=str.length()-1;
-        while(left<=right)
-        {
-            char ch=str.charAt(left);
-            char ch1=str.charAt(right);
-            if(ch!=ch1)
-            {
-                return false;
-            }
-            left++;
-            right--;
+        int reversedHalf=0;
+        while (x>reversedHalf) {
+            reversedHalf=reversedHalf*10+x%10;
+            x=x/10;
         }
-        return true;
+        return (x==reversedHalf||x==reversedHalf/10);
     }
 }
