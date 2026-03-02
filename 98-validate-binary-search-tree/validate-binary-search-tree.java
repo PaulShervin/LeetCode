f31af52801fb3d5c80677e2ab8abd57be88b0727
PaulShervin <paulshervin77@gmail.com>
2026-03-02 17:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (90.77%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -5,9 +5,7 @@ class Solution {
 
     private boolean valid(TreeNode node,long min,long max) {
         if (node==null) return true;
-
         if (!(node.val>min&&node.val<max)) return false;
-
         return valid(node.left,min,node.val)&&valid(node.right, node.val, max);
     }    
 }
