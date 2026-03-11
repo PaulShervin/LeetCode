6a64122a0e5ce9e0c2cda89a3be6a6e67adea8e2
PaulShervin <paulshervin77@gmail.com>
2026-03-11 18:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.45%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0)
+        {
+            return 1;
+        }
+        int no_of_bit=32-Integer.numberOfLeadingZeros(n);
+        int answer=(1<<no_of_bit)-1;
+        n=n^answer;
+        return n;
+    }
+}
