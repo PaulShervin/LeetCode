6bde69ef35cc41ec5cb3a78d016190241a0dcc43
PaulShervin <paulshervin77@gmail.com>
2026-03-11 18:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (94.73%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int n) {
+        if(n==0)
+        {
+            return 1;
+        }
+        int no_of_bit=32-Integer.numberOfLeadingZeros(n);
+        int answer=(1<<no_of_bit)-1;
+        answer^=n;;
+        return answer;
+    }
+}
