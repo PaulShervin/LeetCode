1312671c67f6bcd69ab5f89c0dad3fe4521047d7
PaulShervin <paulshervin77@gmail.com>
2026-05-03 15:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (90.55%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        long exp=n;
-        while(exp<0)
-        {
-            exp=-exp;
-            x=1.0/x;
-        }
-        double base=x;
-        double res=1.0;
-        while(exp>0)
-        {
-            if(exp%2==1)
-            {
-                res*=base;
-            }
-            base*=base;
-            exp/=2;
-        }
-        return res;
+        return Math.pow(x,n);
     }
 }
