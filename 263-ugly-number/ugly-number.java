048bc93945513979db62ba7d9bc8cf22b15b319d
PaulShervin <paulshervin77@gmail.com>
2026-05-14 21:46:29 +0530

    Time: 1 ms (99.52%) | Memory: 42.7 MB (33.07%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -4,17 +4,23 @@ class Solution {
         {
             return  false;
         }
-        while(n%2==0)
+        while(n>1)
         {
-            n/=2;
-        }
-        while(n%3==0)
-        {
-            n/=3;
-        }
-        while(n%5==0)
-        {
-            n/=5;
+            if(n%2==0)
+            {
+                n/=2;
+            }
+            else if(n%3==0)
+            {
+                n/=3;
+            }
+            else if(n%5==0)
+            {
+                n/=5;
+            }
+            else{
+                return false;
+            }
         }
         return n==1;
     }
