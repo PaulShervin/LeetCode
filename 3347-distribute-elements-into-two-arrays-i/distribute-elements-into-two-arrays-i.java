5a943ae912c6785bdb144fe178b6d344258200c4
PaulShervin <paulshervin77@gmail.com>
2026-08-20 19:33:18 +0530

    Time: 1 ms (98.08%) | Memory: 46.3 MB (97.36%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,33 +1,36 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        int n =nums.length;
-        List<Integer> arr1=new ArrayList<>();
-        List<Integer> arr2=new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
-        for(int i=2;i<n;i++)
-        {
-            if(arr1.get(arr1.size()-1)>arr2.get(arr2.size()-1))
-            {
-                arr1.add(nums[i]);
+
+        int n = nums.length;
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        int p1 = 0;
+        int p2 = 0;
+
+        arr1[p1++] = nums[0];
+        arr2[p2++] = nums[1];
+
+        int a = nums[0];
+        int b = nums[1];
+
+        for (int i = 2; i < n; i++) {
+
+            if (a > b) {
+                arr1[p1++] = nums[i];
+                a = nums[i];
+            } else {
+                arr2[p2++] = nums[i];
+                b = nums[i];
             }
-            else if(arr1.get(arr1.size()-1)<arr2.get(arr2.size()-1)){
-                arr2.add(nums[i]);
-            }
         }
-        int[] arr=new int[arr1.size()+arr2.size()];
-        int ind=0;
-        for(int i=0;i<arr1.size();i++)
-        {
-            arr[ind]=arr1.get(i);
-            ind++;
+
+        // Put arr2 after arr1
+        for (int i = 0; i < p2; i++) {
+            arr1[p1++] = arr2[i];
         }
-        System.out.print(arr2.get(arr2.size()-1));
-        for(int i=0;i<arr2.size();i++)
-        {
-            arr[ind]=arr2.get(i);
-            ind++;
-        }
-        return arr;
+
+        return arr1;
     }
 }
