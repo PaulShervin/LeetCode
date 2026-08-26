6b79f685e3baf6201051eafd782990161de66b0b
PaulShervin <paulshervin77@gmail.com>
2026-08-26 16:20:00 +0530

    Time: 2 ms (40.94%) | Memory: 44 MB (75.84%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,47 +1,28 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        Set<String> set=new HashSet<>();
-        for (int i=0;i<s.length();i++) {
-            for (int j=i+1;j<=s.length();j++) {
-                String sub=s.substring(i,j);
-                if (countone(sub)==k){
-                    set.add(sub);
-                }
-            }
-        }
-        int min=Integer.MAX_VALUE;
-        for(String i:set)
-        {
-            int count=i.length();
-            min=Math.min(count,min);
-        }
-        Set<String> New_Set = new HashSet<>(); 
-        for(String i:set)
-        {
-            if(i.length()==min)
-            {
-                New_Set.add(i);
-            }
-        }
-        Set<String> sortedSet = new TreeSet<>(New_Set);
-        for(String i:sortedSet)
-        {
-            return i;
-        }
-        return "";
-        
-    }
-    public int countone(String s)
-    {
-        int count=0;
+        ArrayList<Integer> ones=new ArrayList<>();
         for(int i=0;i<s.length();i++)
         {
-            char ch=s.charAt(i);
-            if(ch=='1')
+            if(s.charAt(i)=='1')
             {
-                count++;
+                ones.add(i);
             }
         }
-        return count;
+        if(ones.size()<k)
+        {
+            return "";
+        }
+        String answer="";
+        for(int i=0;i+k-1<ones.size();i++)
+        {
+            int start=ones.get(i);
+            int end=ones.get(i+k-1);
+            String curr=s.substring(start,end+1);
+            if(answer.equals("") || curr.length()<answer.length()||curr.length()==answer.length() && curr.compareTo(answer)<0)
+            {
+                answer=curr;
+            }
+        }
+        return answer;
     }
 }
