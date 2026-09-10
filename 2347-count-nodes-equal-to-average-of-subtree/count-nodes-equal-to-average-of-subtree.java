9045afcee32e221f2659638877e2cfe431a876e1
PaulShervin <paulshervin77@gmail.com>
2026-09-10 22:05:35 +0530

    Time: 1 ms (56.32%) | Memory: 45.8 MB (24.23%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,35 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    int ans = 0;
-
-    // returns {sum, count}
-    private int[] dfs(TreeNode root) {
-
-        if (root == null) {
-            return new int[]{0, 0};
+    int ans=0;
+    public int[] dfs(TreeNode root)
+    {
+        if(root ==null)
+        {
+            return new int[]{0,0};
         }
-
-        // Get sum and count from left subtree
-        int[] left = dfs(root.left);
-
-        // Get sum and count from right subtree
-        int[] right = dfs(root.right);
-
-        // Calculate current subtree's sum
-        int sum = root.val + left[0] + right[0];
-
-        // Calculate current subtree's node count
-        int count = 1 + left[1] + right[1];
-
-        // Check average
-        if (root.val == sum / count) {
+        int[] left=dfs(root.left);
+        int[] right=dfs(root.right);
+        int sum=root.val+left[0]+right[0];
+        int count=1+left[1]+right[1];
+        if(root.val==sum/count)
+        {
             ans++;
         }
+        return new int[]{sum,count};
 
-        // Return sum and count to parent
-        return new int[]{sum, count};
     }
-
     public int averageOfSubtree(TreeNode root) {
         dfs(root);
         return ans;
